027c69a174059dbc94e00f35e1c4d48396de5852
Nomhle <nomhle.mazibuko@gmail.com>
2024-07-06 22:49:40 +0200

    Added SLF4J dependancy, added some code

--- src/test/java/SauceDemo.java
+++ src/test/java/SauceDemo.java
@@ -11,26 +11,46 @@ public class SauceDemo {
 
     WebDriver driver;
 
-    // this is the testm ethod
+    // this is the test method
     @Test
-    public void loginTests() {
+    public void loginTests() throws InterruptedException {
         driver = new ChromeDriver();
 //        driver= new FirefoxDriver();
 //        driver= new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/"); // This line start the browser
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        Thread.sleep(2000);
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
         String ActualResults = driver.findElement(By.xpath("//span[contains(.,'Products')]")).getText();
         Assert.assertEquals(ActualResults, "Products");
+        Thread.sleep(2000);
+        //ToDo create the code to cater for the rest of the journey
 
-        //ToDo create the coe to cater for the rest of the jurney
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[contains(.,'1')]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Nomhle");
+        Thread.sleep(2000);
+        driver.findElement(By.id("last-name")).sendKeys("Mazibuko");
+        Thread.sleep(2000);
+        driver.findElement(By.id("postal-code")).sendKeys("2012");
+        Thread.sleep(2000);
+        driver.findElement(By.id("continue")).click();
+
+        driver.findElement(By.id("finish")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("back-to-products")).click();
+
 
     }
 
     @AfterTest
-    public void closeBrowser(){
-        driver.quit();
+    public void closeBrowser() {
+        // driver.quit();
     }
 }
