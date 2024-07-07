1864dbbdf56b056723feeb10e9c9e604771bcf2f
Nomhle <nomhle.mazibuko@gmail.com>
2024-07-07 17:27:22 +0200

    added some code

--- src/test/java/SauceDemo.java
+++ src/test/java/SauceDemo.java
@@ -1,8 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
@@ -15,8 +13,6 @@ public class SauceDemo {
     @Test
     public void loginTests() throws InterruptedException {
         driver = new ChromeDriver();
-//        driver= new FirefoxDriver();
-//        driver= new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/"); // This line start the browser
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -30,27 +26,29 @@ public class SauceDemo {
 
 
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//span[contains(.,'1')]")).click();
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
         Thread.sleep(2000);
         driver.findElement(By.id("checkout")).click();
+
         driver.findElement(By.id("first-name")).sendKeys("Nomhle");
         Thread.sleep(2000);
         driver.findElement(By.id("last-name")).sendKeys("Mazibuko");
         Thread.sleep(2000);
-        driver.findElement(By.id("postal-code")).sendKeys("2012");
+        driver.findElement(By.id("postal-code")).sendKeys("1990");
         Thread.sleep(2000);
         driver.findElement(By.id("continue")).click();
-
+        Thread.sleep(2000);
         driver.findElement(By.id("finish")).click();
         Thread.sleep(2000);
         driver.findElement(By.id("back-to-products")).click();
-
-
+        Thread.sleep(2000);
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[contains(@id,'link')][@class='bm-item menu-item'][contains(.,'Logout')]")).click();
     }
 
     @AfterTest
     public void closeBrowser() {
-        // driver.quit();
+        //driver.quit();
     }
 }
