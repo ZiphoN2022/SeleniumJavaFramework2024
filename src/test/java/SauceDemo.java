e0b6e9cb907647883c02251bea1b2d3247aed441
Refilwe007 <123360859+Refilwe007@users.noreply.github.com>
2024-07-08 23:26:07 +0200

    initial commit

--- src/test/java/SauceDemo.java
+++ src/test/java/SauceDemo.java
@@ -11,12 +11,10 @@ public class SauceDemo {
 
     WebDriver driver;
 
-    // this is the testm ethod
+    // this is the testmethod
     @Test
-    public void loginTests() {
+    public void loginTests() throws InterruptedException {
         driver = new ChromeDriver();
-//        driver= new FirefoxDriver();
-//        driver= new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/"); // This line start the browser
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -24,6 +22,19 @@ public class SauceDemo {
         driver.findElement(By.id("login-button")).click();
         String ActualResults = driver.findElement(By.xpath("//span[contains(.,'Products')]")).getText();
         Assert.assertEquals(ActualResults, "Products");
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("shopping_cart_container")).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Mahadi");
+        driver.findElement(By.id("last-name")).sendKeys("New");
+        driver.findElement(By.id("postal-code")).sendKeys("2188");
+        driver.findElement(By.id("continue")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("finish")).click();
+
 
         //ToDo create the coe to cater for the rest of the jurney
 
