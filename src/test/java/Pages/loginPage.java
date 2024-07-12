af9d5da97c0e3282506efce57693bb9788a74d0c
Sinqobile.Tshaka <Sinqobileptshaka@gmial.com>
2024-07-12 12:06:23 +0200

    Created the password and Login button script

--- src/test/java/Pages/loginPage.java
+++ src/test/java/Pages/loginPage.java
@@ -3,7 +3,6 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,21 +14,33 @@ public class loginPage {
 
     @FindBy(xpath = "//input[contains(@id,'user-name')]")
     WebElement username_xpath;
+    @FindBy(xpath = "//input[@id='password']")
+    WebElement password_xpath;
+    @FindBy(xpath = "//input[@id='login-button']")
+    WebElement login_xpath;
 
     //ToDo create code to enter password
     //ToDo create code to click Login button
 
 
-
-    public loginPage(WebDriver driver){
-        this.driver=driver;
+    public loginPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void enterUsername(String username){
+    public void enterUsername(String username) {
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(username_xpath));
         username_xpath.sendKeys(username);
     }
 
+    public void enterPassword(String password) {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(password_xpath));
+        password_xpath.sendKeys(password);
+    }
+
+    public void clickLogin(String login) {
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(login_xpath));
+        login_xpath.click();
 
 
+    }
 }
