55c9811c15fdc27f9cdcf6371ea92955c2d91694
Nhlemamba <nhlemamba@gmail.com>
2024-07-15 21:04:46 +0200

    Added Login and Password paths

--- src/test/java/Pages/loginPage.java
+++ src/test/java/Pages/loginPage.java
@@ -16,10 +16,11 @@ public class loginPage {
     @FindBy(xpath = "//input[contains(@id,'user-name')]")
     WebElement username_xpath;
 
-    //ToDo create code to enter password
-    //ToDo create code to click Login button
+    @FindBy(xpath = "//input[@id='password']")
+    WebElement password_xpath;
 
-
+    @FindBy(xpath = "//input[contains(@id,'login-button')]")
+    WebElement login_xpath;
 
     public loginPage(WebDriver driver){
         this.driver=driver;
@@ -28,6 +29,20 @@ public class loginPage {
     public void enterUsername(String username){
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(username_xpath));
         username_xpath.sendKeys(username);
+    }
+    //ToDo create code to enter password
+
+    public void enterPassword(String password){
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(password_xpath));
+        password_xpath.sendKeys(password);
+    }
+
+    //ToDo create code to click Login button
+
+    public void clickLogin(){
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(login_xpath));
+        login_xpath.click();
+
     }
 
 
