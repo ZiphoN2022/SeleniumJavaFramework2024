0b219e615e7114b3eeecfd121cdcce50c2d30da3
nkosi <nkosi.cele@simplify.africa>
2024-07-16 20:06:24 +0200

    created the first test

--- src/test/java/Tests/loginTests.java
+++ src/test/java/Tests/loginTests.java
@@ -1,4 +1,30 @@
 package Tests;
 
-public class loginTests {
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+@Test
+public class loginTests extends Base{
+
+    public void enterUsernameTests(){
+        loginPage.enterUsername("standard_user");
+    }
+
+    @Test(dependsOnMethods = "enterUsernameTests")
+    public void enterPasswordTests(){
+        loginPage.enterPassword("secret_sauce");
+    }
+    @Test(dependsOnMethods = "enterPasswordTests")
+    public void clickLoginButtonTests(){
+        loginPage.clickLogin();
+    }
+    @Test(dependsOnMethods = "clickLoginButtonTests")
+    public void verfyProductIsDisolayed(){
+        homePage.verifyProductTitleIsDisplayed();
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
 }
