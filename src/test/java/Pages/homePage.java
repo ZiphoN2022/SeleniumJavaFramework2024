b96d801d87006d75c844792e5d7afbc4fb69908e
sgwasira <sgwasira@yahoo.com>
2024-07-16 19:27:07 +0200

    Updated the home and login Pages with find elements code.

--- src/test/java/Pages/homePage.java
+++ src/test/java/Pages/homePage.java
@@ -1,8 +1,14 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class HomePage {
 
@@ -11,11 +17,14 @@ public class HomePage {
     @FindBy(xpath = "//span[@class='title'][contains(.,'Products')]")
     WebElement productTitle_xpath;
 
-    public HomePage(WebDriver driver){
-        this.driver=driver;
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void verifyProductTitleIsDisplayed(){
+    public void verifyProductTitleIsDisplayed() {
 
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(productTitle_xpath));
+        String productTitle = productTitle_xpath.getText();
+        Assert.assertEquals(productTitle, "Products");
     }
 }
--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -1,4 +1,18 @@
 package Utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BrowserFactory {
+
+    static WebDriver driver;
+
+    public  static  WebDriver startBrowser(String browserChoice, String url){
+
+        if(browserChoice.equalsIgnoreCase("Chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+    }
 }
