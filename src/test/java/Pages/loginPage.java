8dcbb8ab6a57a934e142081090cdadbb2b4abc6f
CLEARANKX <NHLANHLACLEARANCE@GMAIL.COM>
2024-07-16 19:28:35 +0200

    Added login credentials

--- src/test/java/Pages/loginPage.java
+++ src/test/java/Pages/loginPage.java
@@ -16,20 +16,36 @@ public class loginPage {
     @FindBy(xpath = "//input[contains(@id,'user-name')]")
     WebElement username_xpath;
 
+    @FindBy(xpath = "//input[contains(@id,'password')]")
+    WebElement password_xpath;
+
+    @FindBy(xpath = "//input[contains(@id,'login-button')]")
+    WebElement login_xpath;
+
+
     //ToDo create code to enter password
     //ToDo create code to click Login button
 
 
-
-    public loginPage(WebDriver driver){
-        this.driver=driver;
+    public loginPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void enterUsername(String username){
+   /* driver.get("https://www.saucedemo.com/");
+    driver.manage().window().maximize();*/
+
+    public void enterUsername(String username) {
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(username_xpath));
         username_xpath.sendKeys(username);
     }
 
+    public void enterPassword(String password) {
+        password_xpath.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        login_xpath.click();
+    }
 
 
 }
