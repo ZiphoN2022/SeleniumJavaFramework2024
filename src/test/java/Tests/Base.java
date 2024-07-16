43863f45fc1d486f4995e68540e8a7a136506c8a
sgwasira <sgwasira@yahoo.com>
2024-07-16 20:12:41 +0200

    Adding the base file

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -4,12 +4,8 @@ import Pages.LoginPage;
 import Utils.BrowserFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-
 import Pages.HomePage;
-import Pages.LoginPage;
-import Utils.BrowserFactory;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
+
 
 public class Base {
 
@@ -17,10 +13,6 @@ public class Base {
 
     final WebDriver driver = browserFactory.startBrowser("chrome","https://www.saucedemo.com");
 
-
-    BrowserFactory browserFactory = new BrowserFactory();
-
-    final WebDriver driver=browserFactory.startBrowser("chrome","https://www.saucedemo.com/");
     LoginPage loginPage = PageFactory.initElements(driver,LoginPage.class);
     HomePage homePage =PageFactory.initElements(driver, HomePage.class);
 
