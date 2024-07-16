25d183624b3c251beca7efa890be60914c163da3
nkosi <nkosi.cele@simplify.africa>
2024-07-16 18:45:20 +0200

    completed the the loginPage and started home page

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,7 +3,6 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,16 +15,18 @@ public class loginPage {
     @FindBy(xpath = "//input[contains(@id,'user-name')]")
     WebElement username_xpath;
 
-    //ToDo create code to enter password
-    @FindBy(xpath = "//input[contains(@id,'user-name')]")
+    @FindBy(xpath = "//input[contains(@id,'password')]")
     WebElement password_xpath;
+
+    @FindBy(id = "login-button")
+    WebElement loginButton_id;
+
+    //ToDo create code to enter password
     //ToDo create code to click Login button
-    @FindBy(xpath = "//input[contains(@id,'user-name')]")
-    WebElement login_xpath;
 
 
 
-    public loginPage(WebDriver driver){
+    public LoginPage(WebDriver driver){
         this.driver=driver;
     }
 
@@ -33,13 +34,16 @@ public class loginPage {
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(username_xpath));
         username_xpath.sendKeys(username);
     }
-   public void password(String password){
-       new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(password_xpath));
-       password_xpath.sendKeys(password);
-   }
-   public void login(){
-       new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(login_xpath));
-       login_xpath.click();
-   }
+
+    public void enterPassword(String password){
+        password_xpath.sendKeys(password);
+    }
+
+    public void clickLogin(){
+        loginButton_id.click();
+    }
+
+
+
 
 }
