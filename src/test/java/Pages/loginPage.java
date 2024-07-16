c9c6d287b52e857df66d35c2a07979eee062065f
Moreblessing <mobychiy@gmail.com>
2024-07-16 18:47:35 +0200

    initial commit

--- src/test/java/Pages/loginPage.java
+++ src/test/java/Pages/loginPage.java
@@ -3,7 +3,6 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,7 +16,12 @@ public class loginPage {
     WebElement username_xpath;
 
     //ToDo create code to enter password
+    @FindBy(xpath = "//input[contains(@id,'password')]")
+    WebElement password_xpath;
+
     //ToDo create code to click Login button
+    @FindBy(xpath = "//input[contains(@id,'login')]")
+    WebElement login_xpath;
 
 
 
@@ -27,7 +31,15 @@ public class loginPage {
 
     public void enterUsername(String username){
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(username_xpath));
-        username_xpath.sendKeys(username);
+        username_xpath.sendKeys();
+    }
+    public void password(String password, WebElement password_xpath){
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(password_xpath));
+        password_xpath.sendKeys();
+    }
+    public void login(){
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(login_xpath));
+        login_xpath.click();
     }
 
 
