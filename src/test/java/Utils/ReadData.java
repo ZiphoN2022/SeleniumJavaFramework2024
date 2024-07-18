4a94bc793d3790099bbd575bc9806b98f083c21e
Nhlemamba <nhlemamba@gmail.com>
2024-07-18 20:11:51 +0200

    my commit

--- src/test/java/Utils/ReadData.java
+++ src/test/java/Utils/ReadData.java
@@ -2,7 +2,7 @@ package Utils;
 
 import org.testng.annotations.Test;
 
-public class ReadData {
+public class gitReadData {
 
     String testDataDir = System.getProperty("user.dir") + "/src/test/java/TestData/TestData.xlsx";
 
